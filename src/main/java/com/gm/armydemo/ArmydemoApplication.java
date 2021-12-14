599c8794dd6435408d5de5613e30b6bffe64afa5
“Gregory <gdc.moser@hotmail.com>
2021-12-14 22:09:10 +0000

    Project created

---
+++ src/main/java/com/gm/armydemo/ArmydemoApplication.java
@@ -0,0 +1,13 @@
+package com.gm.armydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/armydemo/ArmydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.armydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
