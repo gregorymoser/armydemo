7758cac1e47247f2945dfbaa25a5ecfd0ff58bc9
“Gregory <gdc.moser@hotmail.com>
2021-12-16 21:48:30 +0000

    Mapping classes

--- src/main/java/com/gm/armydemo/domain/Military.java
+++ src/main/java/com/gm/armydemo/domain/Military.java
@@ -2,11 +2,19 @@ package com.gm.armydemo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.gm.armydemo.domain.enums.Rank;
 
+@Entity
 public class Military implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String militaryId;
